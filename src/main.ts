ef6fb4956c6a1b913f1c4ded1c8d133ec078760a
Ryan Robertson <ryguyrobertson@gmail.com>
2024-03-17 11:16:35 -0700

    add preload game scene remove template

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
-import { Boot } from './scenes/Boot';
-import { Game as MainGame } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
-import { Preloader } from './scenes/Preloader';
-
 import { Game, Types } from "phaser";
+import { Preloader } from "./scenes/Preloader";
+import { MainGame } from "./scenes/MainGame";
+
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
@@ -13,17 +10,14 @@ const config: Types.Core.GameConfig = {
     width: 1024,
     height: 768,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    // backgroundColor: '#028af8',
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
     scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
+      Preloader,
+      MainGame
     ]
 };
 
---
+++ src/scenes/MainGame.ts
@@ -0,0 +1,23 @@
+import Phaser from "phaser";
+
+// Import Sprites
+
+// Utitilies
+
+// States from Mobx
+
+export class MainGame extends Phaser.Scene {
+  constructor() {
+    super('game');
+  };
+
+
+  preload() {
+    // Cursors here
+  };
+
+  create() {
+
+  };
+
+};
